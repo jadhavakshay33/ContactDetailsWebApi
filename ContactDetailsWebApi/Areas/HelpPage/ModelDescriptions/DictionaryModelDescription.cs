fcaaf406ed4e6831d59f6f5bca1cd289fff89e24
Akshay Jadhav <jadhavakshay4124@gmail.com>
2020-07-18 20:26:16 +0530

    Add project files.

---
+++ ContactDetailsWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ContactDetailsWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ContactDetailsWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContactDetailsWebApi.WebApiApplication" Language="C#" %>
