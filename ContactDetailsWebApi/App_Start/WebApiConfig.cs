b6dc42b545af4f9aa2e3992d5d1e99ef8bcac381
Akshay Jadhav <jadhavakshay4124@gmail.com>
2020-07-20 12:44:34 +0530

    Insert contacts done and display contact done

--- ContactDetailsWebApi/App_Start/WebApiConfig.cs
+++ ContactDetailsWebApi/App_Start/WebApiConfig.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+using System.Net.Http;
+using System.Net.Http.Headers;
 namespace ContactDetailsWebApi
 {
     public static class WebApiConfig
@@ -20,6 +21,7 @@ namespace ContactDetailsWebApi
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
---
+++ ContactDetailsWebApi/Controllers/ContactController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ContactDetailsWebApi.Models;
+
+namespace ContactDetailsWebApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+        [HttpGet]
+        [Route("Contact/HomaePage")]
+        public IHttpActionResult HomePage()
+        {
+            return Ok();
+        }
+
+
+        [Route("Contact/GetAllContactDetails")]
+        public IHttpActionResult GetAllContactDetails()
+        {
+            IList<ContactDetailsViewModel> Contacts = null;
+
+            using (var db = new CallLogDBEntities())
+            {
+                Contacts = db.ContactDetailsTables.Include("Contacts")
+                            .Select(s => new ContactDetailsViewModel()
+                            {
+                                ID=s.ID,
+                                Name=s.Name,
+                                Phone=s.Phone,
+                                Email=s.Email,
+                                CreationDate=s.CreationDate
+                             
+                            }).ToList<ContactDetailsViewModel>();
+            }
+
+            if (Contacts.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(Contacts);
+        }
+        [HttpPost]
+        public IHttpActionResult PostNewContact(ContactDetailsViewModel contact)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var db = new CallLogDBEntities())
+            {
+                db.ContactDetailsTables.Add(new ContactDetailsTable()
+                {
+                    ID = contact.ID,
+                    Name = contact.Name,
+                    Email = contact.Email,
+                    Phone = contact.Phone,
+                    CreationDate = System.DateTime.Today
+                }); ;
+
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
